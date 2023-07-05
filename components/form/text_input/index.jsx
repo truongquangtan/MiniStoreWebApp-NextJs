f5ae98f96a72a7baa2bb93b96a3c4e56feea1ec6
Tien\ASUS <quangtan9bgm@gmail.com>
2023-07-05 13:41:46 +0700

    add fuzzy search in order

--- components/form/text_input/index.jsx
+++ components/form/text_input/index.jsx
@@ -1,10 +1,10 @@
-export default function TextInput({type, name, id, placeholder, value, setValue, onChange, onBlur, ariaInvalid, readOnly, ...props}) {
+export default function TextInput({type, name, id, placeholder, value, onChange, onBlur, ariaInvalid, readOnly, extra, ...props}) {
     return (
         <input
             type={type || "text"}
             id={id}
             name={name}
-            className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5"
+            className={`border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 ${extra}`}
             placeholder={placeholder}
             value={value}
             onChange={onChange}
